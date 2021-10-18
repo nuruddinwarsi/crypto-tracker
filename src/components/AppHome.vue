3cae7e4d055d45ca3f12d551711d190e5f0b5117
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-18 19:51:02 +0530

    add params to getCryptoList API

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -9,10 +9,25 @@ import { getCryptoList } from '@/services/getCryptoList';
 
 export default {
   name: 'AppHome',
+
+  data() {
+    return {
+      // getCryptoList API data
+      vs_currency: 'inr', //price in specified currency
+      order: 'market_cap_desc',
+      per_page: '10', //list 10 cryptos
+      price_change_percentage: '1h', //1h, 24h, 7d, 14d, 30d, 200d,comma seperated accepted
+    };
+  },
   methods: {
     async getCryptoList() {
       try {
-        const data = await getCryptoList();
+        const data = await getCryptoList(
+          this.vs_currency,
+          this.order,
+          this.per_page,
+          this.price_change_percentage
+        );
         console.log(data);
       } catch (error) {
         console.log(error);
