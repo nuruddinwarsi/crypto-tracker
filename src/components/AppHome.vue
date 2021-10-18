481084a72cc9ad4ae8779c10bef190e3de080e2e
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-18 20:38:37 +0530

    get basic data to display on homepage

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1>Homepage</h1>
+  <div class="body-content">
+    <div class="header">
+      <h2>CryptoTracker</h2>
+    </div>
+    <div v-for="crypto in cryptoList" :key="crypto.id">
+      <img :src="crypto.image" :alt="crypto.name" class="coin-image" />
+      <span>{{ crypto.name }}</span>
+      <span> : INR {{ crypto.current_price }}</span>
+      <span> : {{ crypto.price_change_percentage_24h }}%</span>
+    </div>
   </div>
 </template>
 
@@ -17,6 +25,8 @@ export default {
       order: 'market_cap_desc',
       per_page: '10', //list 10 cryptos
       price_change_percentage: '1h', //1h, 24h, 7d, 14d, 30d, 200d,comma seperated accepted
+
+      cryptoList: [],
     };
   },
   methods: {
@@ -28,6 +38,7 @@ export default {
           this.per_page,
           this.price_change_percentage
         );
+        this.cryptoList = data;
         console.log(data);
       } catch (error) {
         console.log(error);
@@ -35,9 +46,19 @@ export default {
     },
   },
   created() {
-    this.getCryptoList();
+    // Call API on page load
+    // this.getCryptoList();
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.header {
+  display: grid;
+  justify-content: center;
+}
+
+.coin-image {
+  width: 10%;
+}
+</style>
