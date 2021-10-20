875f92a7c94920b73af0b87c033816117db4e236
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-20 14:49:46 +0530

    Created CryptoCard component

---
+++ src/components/CryptoCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="crypto">
+    <img :src="crypto.image" :alt="crypto.name" class="coin-image" />
+    <div class="name">{{ crypto.name }}</div>
+    <div class="price">{{ crypto.current_price }}</div>
+    <div class="change">
+      <span>{{ crypto.price_change_24h }}</span>
+      <span>{{ crypto.price_change_percentage_24h }}%</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CryptoCard',
+  props: {
+    crypto: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  filter: {},
+};
+</script>
+
+<style scoped>
+.crypto {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: auto;
+  /* row-gap: 1000px; */
+  /* grid-template-areas:
+    'coin-image name price'
+    'coin-image name change'; */
+  padding: 10px;
+  border-bottom: 0.1px solid grey;
+  font-size: 14px;
+}
+
+.name {
+  grid-area: 'name';
+}
+.price {
+  grid-area: 'price';
+}
+.change {
+  display: grid;
+  grid-area: 'change';
+}
+
+.coin-image {
+  grid-area: 'coin-image';
+  width: 35%;
+}
+</style>
