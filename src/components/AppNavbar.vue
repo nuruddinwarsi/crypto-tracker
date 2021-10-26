eb5eec255b72a6604cd1400392a5bf91882b768c
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-27 03:45:29 +0530

    show login nav menu if logged out

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,33 +1,63 @@
 <template>
   <div class="navbar">
-    <router-link class="navbar-link item-1" exact to="/" active-class="active">
-      <font-awesome-icon icon="home" class="icon" /> Home
-    </router-link>
-    <router-link
-      class="navbar-link item-2"
-      to="/portfolio"
-      active-class="active"
-    >
-      <font-awesome-icon icon="user" class="icon" />Portfolio</router-link
-    >
-    <router-link class="navbar-link item-3" to="/alerts" active-class="active">
-      <font-awesome-icon icon="bell" class="icon" />Alerts</router-link
-    >
+    <div>
+      <router-link
+        class="navbar-link item-1"
+        exact
+        to="/"
+        active-class="active"
+      >
+        <font-awesome-icon icon="home" class="icon" /> Home
+      </router-link>
+    </div>
+    <div>
+      <router-link
+        class="navbar-link item-2"
+        to="/portfolio"
+        active-class="active"
+      >
+        <font-awesome-icon
+          :icon="['fab', 'bitcoin']"
+          class="icon"
+        />Portfolio</router-link
+      >
+    </div>
+    <div>
+      <router-link
+        class="navbar-link item-3"
+        to="/alerts"
+        active-class="active"
+      >
+        <font-awesome-icon icon="bell" class="icon" />Alerts</router-link
+      >
+    </div>
+
+    <div v-if="!isAuthenticated">
+      <router-link class="navbar-link item-4" to="/login" active-class="active">
+        <font-awesome-icon icon="user" class="icon" />Login</router-link
+      >
+    </div>
   </div>
 </template>
 
 <script>
+import store from '@/stores';
+
 export default {
   name: 'AppNavbar',
+  computed: {
+    isAuthenticated() {
+      return store.getters.isAuthenticated;
+    },
+  },
 };
 </script>
 
 <style scoped>
 .navbar {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
   grid-template-rows: 1fr;
-  grid-template-areas: 'item-1 item-2 item-3';
+  grid-auto-flow: column;
   justify-items: stretch;
   border-radius: 5px 5px 0 0;
   background-color: #202020;
@@ -38,21 +68,10 @@ export default {
   padding: 0 8px;
 }
 
-.item-1 {
-  grid-area: item-1;
-}
-.item-2 {
-  grid-area: item-2;
-}
-.item-3 {
-  grid-area: item-3;
-  color: #febc1d;
-}
-
 .navbar-link {
   text-decoration: none;
   color: #8edce6;
-  padding: 20px;
+  padding: 16px 0;
   display: grid;
   grid-template-rows: repeat(2, 0.5fr);
   justify-items: center;
@@ -71,35 +90,5 @@ export default {
     200%;
   color: white;
   border-radius: 5px;
-}
-
-@media only screen and (min-width: 500px) {
-  /* .navbar {
-    height: 100%;
-    grid-template-rows: repeat(3, 0.1fr);
-    grid-template-columns: 1fr;
-    grid-template-areas:
-      'item-1'
-      'item-2'
-      'item-3';
-    border-radius: 0 5px 0 0;
-  }
-  .navbar-link {
-    grid-template-rows: 1fr;
-    grid-template-columns: 0.1fr 0.9fr;
-  }
-
-  .navbar-link:hover,
-  .navbar-link:focus {
-    border-bottom: none;
-    border-left: 2px solid #a1c4fd;
-    color: white;
-    border-radius: 5px;
-  } */
-
-  /* .active {
-    border-bottom: none;
-    border-left: 2px solid #8edce6;
-  } */
 }
 </style>
--- src/init.js
+++ src/init.js
@@ -17,9 +17,9 @@ Vue.component('apexchart', VueApexCharts);
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHome, faUser, faBell } from '@fortawesome/free-solid-svg-icons';
-import { faBitbucket } from '@fortawesome/free-brands-svg-icons';
+import { faBitcoin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faHome, faUser, faBitbucket, faBell);
+library.add(faHome, faUser, faBitcoin, faBell);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
