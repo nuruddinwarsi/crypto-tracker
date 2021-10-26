493401a3af689b80bc251041f6cfa34eff43d157
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-27 05:22:26 +0530

    set correct env variable

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const AppConfig = {
   cryptoApi: process.env.VUE_APP_CRYPTO_API,
   // apiBaseUrl: 'https://crypt0-portfolio-tracker.herokuapp.com/api',
-  apiBaseUrl: process.env.VUE_APP_LOCAL_URL,
+  apiBaseUrl: process.env.VUE_APP_BACKEND_API,
 };
 
 export default AppConfig;
