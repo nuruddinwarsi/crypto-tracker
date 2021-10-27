62d4feef5fbc053207593a1ff8b59d6455293650
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-27 18:57:00 +0530

    create groups for each coin

--- src/components/AppPortfolio.vue
+++ src/components/AppPortfolio.vue
@@ -1,32 +1,64 @@
 <template>
   <div class="body-content">
     <PageHeader :header="`Portfolio`" />
+    <div class="portfolio">
+      <div v-for="(coinGroup, index) in coinData" :key="index">
+        <CoinGroup :coinGroup="coinGroup" />
+        <hr />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import PageHeader from '@/components/utils/PageHeader';
 import { getPortfolio } from '@/services/getPortfolio';
+import CoinGroup from '@/components/CoinGroup';
 
 export default {
   name: 'AppPortfolio',
   components: {
     PageHeader,
+    CoinGroup,
+  },
+  data() {
+    return {
+      portfolioData: '',
+      i: 0,
+      coinData: {},
+    };
   },
   methods: {
     async callGetPortfolio() {
       try {
-        const data = await getPortfolio();
-        console.log(data);
+        const response = await getPortfolio();
+        this.portfolioData = response.data;
+        this.createPortfolioSummary(this.portfolioData);
       } catch (error) {
         console.log(error);
       }
     },
+    createPortfolioSummary(portfolioData) {
+      this.coinData = portfolioData.reduce(
+        (acc, cur) => ({
+          ...acc,
+          [cur.coinId]: [...(acc[cur.coinId] || []), cur],
+        }),
+        {}
+      );
+      console.log(this.coinData);
+    },
   },
+
   created() {
     this.callGetPortfolio();
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.body-content {
+  grid-template-columns: 1fr;
+  grid-template-rows: 0.1fr 1fr;
+}
+</style>
---
+++ src/components/CoinGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="coinGroup">
+    <div v-for="(coin, index) in coinGroup" :key="index">
+      {{ coin }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CoinGroup',
+  props: {
+    coinGroup: {
+      type: Array,
+    },
+  },
+};
+</script>
+<style scoped>
+.coinGroup {
+  border: 1px solid red;
+}
+</style>
