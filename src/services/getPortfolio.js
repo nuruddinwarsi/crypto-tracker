a0e266aca717c16657cfbc52ef8597c746303cda
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-10-28 17:16:09 +0530

    use auth header

--- src/services/getPortfolio.js
+++ src/services/getPortfolio.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 import AppConfig from '@/config';
+import Vue from 'vue';
 
 const { apiBaseUrl } = AppConfig;
 
+const jwtToken = Vue.$cookies.get('jwt');
+const config = {
+  headers: {
+    Authorization: jwtToken,
+  },
+};
 const getPortfolio = async () => {
-  const response = await axios.get(`${apiBaseUrl}/v1/getPortfolio`, {
-    withCredentials: true,
-  });
+  const response = await axios.get(`${apiBaseUrl}/v1/getPortfolio`, config);
   return response.data;
 };
 
