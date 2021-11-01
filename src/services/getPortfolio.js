aed989d70287c33ffb81e07878054a3a97d68ff9
Nuruddin Warsi <nuruddinwarsi89@gmail.com>
2021-11-02 04:22:30 +0530

    getPortfolio - send auth token

--- src/services/getPortfolio.js
+++ src/services/getPortfolio.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 import AppConfig from '@/config';
-// import Vue from 'vue';
+import Vue from 'vue';
 
 const { apiBaseUrl } = AppConfig;
 
-// const jwtToken = Vue.$cookies.get('jwt');
-// const config = {
-//   headers: {
-//     Authorization: jwtToken,
-//   },
-// };
+let jwtToken = Vue.$cookies.get('jwt');
+
 const getPortfolio = async () => {
   const response = await axios.get(`${apiBaseUrl}/v1/getPortfolio`, {
+    headers: {
+      Authorization: jwtToken,
+    },
     withCredentials: true,
   });
   return response.data;
